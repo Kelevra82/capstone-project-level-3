c5ff8bbb8dc640491e7818a215ca606de3f045f8
kelevra82 <tdmatte82@gmail.com>
2024-09-29 02:14:36 +0000

    corrections

--- utility-functions.js
+++ utility-functions.js
@@ -1,10 +1,15 @@
 function convertCard(cardObject) {
-  let cardString = '<div class="card">';
-  cardString += '<img class="card-img-top" src="' + cardObject.src + '"/>';
-  cardString += '<div class="card-body">';
-  cardString += '<h3 class="card-title">' + cardObject.title + '</h3>';
-  cardString += cardObject.description;
-  cardString += '</div>';
-  cardString += '</div>';
-  return cardString;
-}
+    let cardString = '<div class="card text-black">';
+    cardString += '<img class="card-img-top" src="' + cardObject.src + '"/>';
+    cardString += '<div class="card-body">';
+    cardString += '<h3 class="card-title">' + cardObject.title + '</h3>';
+    cardString += cardObject.description;
+    cardString += '</div>';
+    cardString += '</div>';
+    return cardString;
+  }
+
+  function submitBtn() {
+    const msgSent = "Your message has been sent!";
+      window.outputTag.innerHTML += msgSent;
+    }
