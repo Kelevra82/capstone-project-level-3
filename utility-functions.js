52da863ce091c5c54dcc52199c38aa2600fff660
kelevra82 <tdmatte82@gmail.com>
2024-10-11 07:10:13 +0000

    🦂🍵 Checkpoint ./about.html:69253285/10270 ./utility-functions.js:69253285/523

--- utility-functions.js
+++ utility-functions.js
@@ -1,5 +1,5 @@
 function convertCard(cardObject) {
-    let cardString = '<div class="card text-black">';
+    let cardString = '<div class="card bg-dark text-primary fs-5">';
     cardString += '<img class="card-img-top" src="' + cardObject.src + '"/>';
     cardString += '<div class="card-body">';
     cardString += '<h3 class="card-title">' + cardObject.title + '</h3>';
